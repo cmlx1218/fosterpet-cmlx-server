8c1154f1deb4a482d47da51bdf1e60c50f881af0
cmlx <cmlx1218@163.com>
2019-06-20 14:43:35 +0800

    feat:配置拉取配置中心文件

--- fosterpet-config/src/main/java/com/fosterpet/cmlx/fosterpetconfig/FosterpetConfigApplication.java
+++ fosterpet-config/src/main/java/com/fosterpet/cmlx/fosterpetconfig/FosterpetConfigApplication.java
@@ -3,14 +3,23 @@ package com.fosterpet.cmlx.fosterpetconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableConfigServer
-public class FosterpetConfigApplication {
+@EnableConfigServer
+public class FosterpetConfigApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(FosterpetConfigApplication.class, args);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.APPLICATION_JSON);
     }
 
 }
